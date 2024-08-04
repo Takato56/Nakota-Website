0e67e61e2a8de0c1aa156003282c3a556c5a2ceb
nvnhatt201204 <mottongmotdep9@gmail.com>
2024-08-04 23:13:11 +0700

    Demo Website

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,3 +10,22 @@ buyButtons.forEach(button => {
     alert('Sản phẩm đã được thêm vào giỏ hàng!');
   });
 });
+
+
+// intersection observer API cho class "intersection"
+
+// const observer = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       const animationDirection = entry.target.getAttribute('data-animation')
+//       entry.target.classList.add(`${animationDirection}-visible`)
+//       observer.unobserve(entry.target);
+//     }
+//   })
+// })
+
+// const elements = document.querySelectorAll('[data-animation]');
+// elements.forEach(element => {
+//   observer.observe(element);
+// })
+
